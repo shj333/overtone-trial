71b5bda3ba9e9c0756c0f1db06343b8931a9c530
stuart <stuartj@berwickheights.com>
2015-02-12 09:38:51 -0500

    More work on FM (Dodge Jerse Chapter 5)

--- src/berwickheights/dodge_jerse/chapter5.clj
+++ src/berwickheights/dodge_jerse/chapter5.clj
@@ -28,6 +28,24 @@
          (at this-time (basic-fm :index-of-mod this-idx-of-mod :dur this-dur)))
        (take 5 (iterate inc 1))))
 
-; Figure 5.6, inharmonic spectrum
-(basic-fm :car-freq 100 :mod-freq (* 1.4 100) :index-of-mod 1 :dur 3)
+; Figure 5.6, inharmonic spectrum at 5 different frequencies (ratio of fc:fm ~= 1:sqrt(2))
+(let [n (now)]
+  (map #(let [this-car-freq (* % 100)
+              this-mod-freq (* 1.4 this-car-freq)
+              this-dur 3
+              this-time (+ n 1000 (* 1000 (- % 1) this-dur))]
+         (at this-time (basic-fm :car-freq this-car-freq :mod-freq this-mod-freq :index-of-mod 1 :dur this-dur :amp 1.0)))
+       (take 5 (iterate inc 1))))
 
+; Figure 5.7, simple instrument with dynamic spectra
+; Note: Try this using different envs for the mod-freq-env (env-lin, env-sine, env-triangle, etc)
+(definst dyn-spec-fm [car-freq 440 mod-freq 250 index-of-mod 2 amp 0.5 dur 5]
+         (let [mod-freq-env (env-gen (env-perc :release dur) :action FREE)
+               deviation (* mod-freq index-of-mod mod-freq-env)
+               modulator (+ car-freq (* deviation (sin-osc mod-freq)))
+               amp-env (env-gen (env-perc :release dur) :action FREE)]
+           (* amp amp-env (sin-osc modulator))))
+
+; Examples of above, note how spectra changes over time in dyn-spec-fm, but stays same in basic-rm
+(basic-fm :car-freq 200 :mod-freq 280 :index-of-mod 5)
+(dyn-spec-fm :car-freq 200 :mod-freq 280 :index-of-mod 5)
