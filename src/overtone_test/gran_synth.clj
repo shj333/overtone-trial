1704b4b25189329ad30123cf4a387122b17a2211
stuart <stuartj@berwickheights.com>
2016-01-15 11:17:50 -0500

    Fixed typo

--- src/overtone_test/gran_synth.clj
+++ src/overtone_test/gran_synth.clj
@@ -1,7 +1,5 @@
-(ns
-  ^{:author stuart}
-  overtone_test
-  (use overtone.live))
+(ns overtone-test.gran-synth
+  (:use overtone.live))
 
 (defsynth gran-synth-test [out-chan 0]
   (let [src (grain-buf )]
