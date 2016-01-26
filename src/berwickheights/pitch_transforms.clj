ebf107cfafe92ac16b6f5d0231971973bec084a2
stuart <stuartj@berwickheights.com>
2016-01-25 22:26:09 -0500

    More work on Boulez multiplications

--- src/berwickheights/pitch_transforms.clj
+++ src/berwickheights/pitch_transforms.clj
@@ -1,34 +1,43 @@
 (ns berwickheights.pitch-transforms)
 
 (def pitch-names [:C :Db :D :Eb :E :F :Gb :G :Ab :A :Bb :B])
-(def pitch-nums { :C 0 :Db 1 :D 2 :Eb 3 :E 4 :F 5 :Gb 6 :G 7 :Ab 8 :A 9 :Bb 10 :B 11})
+(def pitch-nums {:C 0 :Db 1 :D 2 :Eb 3 :E 4 :F 5 :Gb 6 :G 7 :Ab 8 :A 9 :Bb 10 :B 11})
 
 (defn transpose-pitch [pitch level] (mod (+ pitch level) 12))
 (defn transpose [set level] (map #(transpose-pitch % level) set))
 (defn transpose-many [set levels] (map #(transpose set %) levels))
-(defn boulez-mult [set1 set2]
-  (let [set1-num (map pitch-nums set1)
-        set2-num (map pitch-nums set2)
+
+(defn numeric-set [set] (if (keyword? (first set)) (map pitch-nums set) set))
+(defn named-set [set] (if (keyword? (first set)) set (map pitch-names set)))
+
+(defn boulez-multi [set1 set2]
+  (let [set1-num (numeric-set set1)
+        set2-num (numeric-set set2)
         first-pitch (first set2-num)
         set2-zeroed (map #(- % first-pitch) set2-num)]
-    (sort (distinct (apply concat (transpose-many set1-num set2-zeroed))))))
+    (-> (transpose-many set1-num set2-zeroed)
+        flatten
+        distinct
+        sort)))
 
 ; From Boulez On Music Today, page 40
-(def example-multi-set [:Ab :C :A :B])
-(map pitch-names (transpose (boulez-mult [:D :F :Eb] example-multi-set) 6))
-(map pitch-names (transpose (boulez-mult [:Bb :Db] example-multi-set) 6))
-(map pitch-names (transpose (boulez-mult example-multi-set example-multi-set) 6))
-(map pitch-names (transpose (boulez-mult [:E :G] example-multi-set) 6))
-(map pitch-names (transpose (boulez-mult [:Gb] example-multi-set) 6))
+(def example-multi-src-set [:Ab :C :A :B])
+(def example-multi-parts [[:D :F :Eb] [:Bb :Db] example-multi-src-set [:E :G] [:Gb]])
+(defn boulez-example [set transpose-level]
+  (-> (boulez-multi set example-multi-src-set)
+      (transpose transpose-level)
+      named-set))
+(map #(boulez-example % 6) example-multi-parts)
+(map #(boulez-example % 9) example-multi-parts)
+(map #(boulez-example % 7) example-multi-parts)
 
-(map pitch-names (transpose (boulez-mult [:D :F :Eb] example-multi-set) 9))
-(map pitch-names (transpose (boulez-mult [:Bb :Db] example-multi-set) 9))
-(map pitch-names (transpose (boulez-mult example-multi-set example-multi-set) 9))
-(map pitch-names (transpose (boulez-mult [:E :G] example-multi-set) 9))
-(map pitch-names (transpose (boulez-mult [:Gb] example-multi-set) 9))
 
-(map pitch-names (transpose (boulez-mult [:D :F :Eb] example-multi-set) 7))
-(map pitch-names (transpose (boulez-mult [:Bb :Db] example-multi-set) 7))
-(map pitch-names (transpose (boulez-mult example-multi-set example-multi-set) 7))
-(map pitch-names (transpose (boulez-mult [:E :G] example-multi-set) 7))
-(map pitch-names (transpose (boulez-mult [:Gb] example-multi-set) 7))
+(defn map-pitch-to-freq [pc pitch-oct-map]
+  (let [midi (+ 60 pc)]
+    (+ midi (* 12 (pitch-oct-map pc)))))
+
+(defn map-set-to-freq [set pitch-oct-map]
+  (let [set-num (numeric-set set)]
+    (map #(map-pitch-to-freq % pitch-oct-map) set-num)))
+
+(def pitch-oct-map1 [0 1 -1 2 -2 3 -3 0 1 -2 -3 -4])
