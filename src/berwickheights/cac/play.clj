c553cda58b16dc196f9646384f3e16c09a394076
stuart <stuartj@berwickheights.com>
2016-01-27 10:08:34 -0500

    Remove Overtone dependencies from berwickheights.cac.play

--- src/berwickheights/cac/play.clj
+++ src/berwickheights/cac/play.clj
@@ -1,17 +1,16 @@
 (ns berwickheights.cac.play
-  (:require [berwickheights.cac.pitch-transforms :as pt])
-  (:require [overtone.core :as ot])
-  (:require [overtone.inst.piano :as pno]))
+  (:require [berwickheights.cac.pitch-transforms :as pt]))
 
 
 (defn play-set
   "Plays the given set of pitch classes by mapping the set using the function map-set-to-octaves() with
-  a given octave map. Uses the Overtone piano function to play the pitches in the resulting set.
+  a given octave map. Uses the given instrument to play the pitches in the resulting set by converting
+  each pitch with the given note-func.
 
   Example:
   (def pitch-oct-map [4 5 3 6 2 7 1 4 5 2 1 8])
   (play-set [0 1 3] pitch-oct-map)
   "
-  [set octave-map]
-  (let [notes (map ot/note (pt/map-set-to-octaves set octave-map))]
-    (doseq [note notes] (pno/piano note))))
+  [inst note-func set octave-map]
+  (let [notes (map note-func (pt/map-set-to-octaves set octave-map))]
+    (doseq [note notes] (inst note))))
