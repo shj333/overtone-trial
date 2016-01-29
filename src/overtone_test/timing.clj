4067a3aacf08c21054864b62dd9369df21065551
stuart <stuartj@berwickheights.com>
2016-01-29 09:56:02 -0500

    Timing test now uses new play-phrase function

--- src/overtone_test/timing.clj
+++ src/overtone_test/timing.clj
@@ -1,5 +1,6 @@
 (ns overtone-test.timing
-  (:use overtone.core))
+  (:use overtone.core)
+  (:require [berwickheights.cac.play :as play]))
 
 
 ; (boot-external-server)
@@ -34,36 +35,33 @@
 ; Music
 ;
 (defn play-gong-seq [nome gong-data]
-  (let [beat (nome)]
-    (at (nome beat) (play-gong-cluster gong-data 0.5))
-    (at (nome (+ 1 beat)) (play-gong-cluster gong-data 0.25))
-    (at (nome (+ 2 beat)) (play-gong-cluster gong-data 0.3))
-    (at (nome (+ 3.5 beat)) (play-gong-cluster gong-data 0.4))
-    (at (nome (+ 3.75 beat)) (play-gong-cluster gong-data 0.14))))
+  (let [phrase [[0 [gong-data 0.5]]
+                [1 [gong-data 0.25]]
+                [2 [gong-data 0.3]]
+                [3.5 [gong-data 0.4]]
+                [3.75 [gong-data 0.14]]]]
+    (play/play-phrase play-gong-cluster phrase nome)))
 
 (defn play-perc-seq [nome]
-  (let [beat (+ (nome) 0.6)]
-    (at (nome (+ 1 beat)) (pitch-perc :cf 2000 :amp 4.0))
-    (at (nome (+ 1.125 beat)) (pitch-perc :cf 2100 :amp 3.0))
-    (at (nome (+ 1.25 beat)) (pitch-perc :cf 2300 :amp 3.0))
-    (at (nome (+ 1.375 beat)) (pitch-perc :cf 1900 :amp 4.0))
-    (at (nome (+ 1.75 beat)) (pitch-perc :cf 2700 :amp 6.0))
-    (at (nome (+ 2.1 beat)) (pitch-perc :cf 1300 :amp 6.0))))
+  (let [phrase [[1 [:cf 2000 :amp 4.0]]
+                [1.125 [:cf 2100 :amp 3.0]]
+                [1.25 [:cf 2300 :amp 3.0]]
+                [1.375 [:cf 1900 :amp 4.0]]
+                [1.75 [:cf 2700 :amp 6.0]]
+                [2.1 [:cf 1300 :amp 6.0]]]]
+    (play/play-phrase pitch-perc phrase nome 0.6)))
 
-(defn section1 [nome gong-data]
-  (play-gong-seq nome gong-data)
-  (play-perc-seq nome))
+(defn section1 [nome gong-freqs gong-durs]
+  (let [gong-data (->> (map vector (repeat :freq) (map midi->hz gong-freqs) (repeat :dur) gong-durs)
+                       (map #(apply hash-map %)))]
+    (play-gong-seq nome gong-data)
+    (play-perc-seq nome)))
 
 (def nome (metronome 44))
-(let [gong-data [{:freq (midi->hz 74) :dur 8}
-                 {:freq (midi->hz 61) :dur 6}
-                 {:freq (midi->hz 70) :dur 10}
-                 {:freq (midi->hz 81) :dur 12}
-                 {:freq (midi->hz 76) :dur 5}]]
-  (section1 nome gong-data))
-(let [lowest-gong-midi 70
-      durs [8 6 10 12 5]
-      freqs (map #(midi->hz (+ lowest-gong-midi %)) (range))
-      all-data (map vector (repeat :freq) freqs (repeat :dur) durs)
-      gong-data (map #(apply hash-map %) all-data)]
-  (section1 nome gong-data))
+
+; (section1 nome [74 61 70 81 76] [8 6 10 12 5])
+
+;(let [lowest-gong-midi 70
+;      freqs (map + (repeat lowest-gong-midi) (range))
+;      durs [8 6 10 12 5]]
+;  (section1 nome freqs durs))
