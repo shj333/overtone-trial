146807ac4fe1971cf39546be29e1e75c698980af
stuart <stuartj@berwickheights.com>
2016-01-31 18:46:56 -0500

    More doc

--- src/berwickheights/notes_metalevel/chapter17.clj
+++ src/berwickheights/notes_metalevel/chapter17.clj
@@ -6,6 +6,7 @@
 ; Notes from the Metalevel, Chapter 17: Chaos
 ;
 
+; See https://en.wikipedia.org/wiki/Logistic_map
 (defn logistic-map
   "Returns lazy seq according to y(s, c) = c * y * (1 - y). From Chapter 17 of Notes from the Metalevel."
   ([chaos] (logistic-map (rand 1.0) chaos))
@@ -23,4 +24,21 @@
    (let [seq (map #(scale-range % 0 1.0 low high) (take n (logistic-map chaos)))]
      (play/play-sequence n seq nome instr instr-vals beat-dur))))
 
+; (volume 0.2)
+; (use 'overtone.inst.sampled-piano)
+; (def pno sampled-piano)
+; (def nome (metronome 120))
+;
+; Converge to one note
+; (play-logistic-map 40 2.8 60 72 nome pno [:sustain 0.1] 0.125)
+;
+; Converge to four notes
+; (play-logistic-map 40 3.5 60 72 nome pno [:sustain 0.1] 0.125)
+;
+; Chaos
+; Notes range from midi-low to midi-high on sampled piano
 ; (play-logistic-map 100 3.7 21 109 nome pno [:sustain 0.1] 0.125)
+;
+; Notes range from middle C to one octave up on sampled piano
+; (play-logistic-map 100 3.7 60 72 nome pno [:sustain 0.1] 0.125)
+;
