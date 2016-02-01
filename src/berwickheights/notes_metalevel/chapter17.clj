24be9a6a4d4157f53fd94834abee479bcf720e64
stuart <stuartj@berwickheights.com>
2016-01-31 20:19:51 -0500

    Added Henon Map function to Notes from hte Metalevel, Chap 17

--- src/berwickheights/notes_metalevel/chapter17.clj
+++ src/berwickheights/notes_metalevel/chapter17.clj
@@ -1,6 +1,7 @@
 (ns berwickheights.notes-metalevel.chapter17
-  (:require [berwickheights.cac.play :as play])
-  (:use overtone.core))
+  (:use [overtone.core]
+        [overtone.inst.sampled-piano])
+  (:require [berwickheights.cac.play :as play]))
 
 ;
 ; Notes from the Metalevel, Chapter 17: Chaos
@@ -8,37 +9,41 @@
 
 ; See https://en.wikipedia.org/wiki/Logistic_map
 (defn logistic-map
-  "Returns lazy seq according to y(s, c) = c * y * (1 - y). From Chapter 17 of Notes from the Metalevel."
-  ([chaos] (logistic-map (rand 1.0) chaos))
-  ([prev-val chaos]
+  "Returns lazy seq according to new-val = r * old-val * (1 - old-val). From Chapter 17 of Notes from the Metalevel.
+  R vals:
+    1-3: converges to one note
+    3-3.45: converges to two notes
+    3.45-3.54: converges to four notes
+    3.54-3.56: converges to 8, 16, 32 notes
+    > 3.56: chaos"
+  ([r] (logistic-map (rand 1.0) r))
+  ([prev-val r]
    (lazy-seq
-     (let [new-val (* chaos prev-val (- 1 prev-val))]
-       (cons new-val (logistic-map new-val chaos))))))
+     (let [new-val (* r prev-val (- 1 prev-val))]
+       (cons new-val (logistic-map new-val r))))))
 
 (defn play-logistic-map
-  "Play n notes from logistic map sequence using the given instrument and metronome. The
-  logistic map generates numbers using the given chaos constant. Notes are mapped from the
-  logistic map sequence by scaling them betwen the given low and high."
-  ([n chaos low high nome instr instr-vals] (play-logistic-map n chaos low high nome instr instr-vals 0.125))
-  ([n chaos low high nome instr instr-vals beat-dur]
-   (let [seq (map #(scale-range % 0 1.0 low high) (take n (logistic-map chaos)))]
-     (play/play-sequence n seq nome instr instr-vals beat-dur))))
+  [n r]
+  (let [nome (metronome 120)
+        pno sampled-piano
+        seq (logistic-map r)]
+    (play/play-sequence n seq 0 1.0 60 72 nome pno [:sustain 0.1] 0.125)))
 
-; (volume 0.2)
-; (use 'overtone.inst.sampled-piano)
-; (def pno sampled-piano)
-; (def nome (metronome 120))
-;
-; Converge to one note
-; (play-logistic-map 40 2.8 60 72 nome pno [:sustain 0.1] 0.125)
-;
-; Converge to four notes
-; (play-logistic-map 40 3.5 60 72 nome pno [:sustain 0.1] 0.125)
-;
-; Chaos
-; Notes range from midi-low to midi-high on sampled piano
-; (play-logistic-map 100 3.7 21 109 nome pno [:sustain 0.1] 0.125)
-;
-; Notes range from middle C to one octave up on sampled piano
-; (play-logistic-map 100 3.7 60 72 nome pno [:sustain 0.1] 0.125)
-;
+
+(defn henon-map
+  "Returns lazy seq of duples according to H(x, y) = ((y + 1) - ax2, bx), where a and b are constants (typically
+  1.4 and 0.3). From Chapter 17 of Notes from the Metalevel."
+  ([] (henon-map 1.4 0.3))
+  ([a b] (henon-map 0 0 a b))
+  ([x y a b]
+   (lazy-seq
+     (let [new-x (- (+ y 1) (* a x x))
+           new-y (* b x)]
+       (cons [new-x new-y] (henon-map new-x new-y a b))))))
+
+(defn play-henon-map
+  [n]
+  (let [nome (metronome 120)
+        pno sampled-piano
+        seq (map first (henon-map))]
+    (play/play-sequence n seq -5 5 21 108 nome pno [:sustain 0.1] 0.125)))
