112691403377c1dba7e1436267ccc4e8737910f7
stuart <stuartj@berwickheights.com>
2016-02-02 10:45:20 -0500

    More work on probability library (WIP, will incorporate Apache Commons Math3 lib)

--- src/berwickheights/cac/probability.clj
+++ src/berwickheights/cac/probability.clj
@@ -1,19 +1,29 @@
-(ns berwickheights.cac.probability)
+(ns berwickheights.cac.probability
+  (:import (org.apache.commons.math3.random RandomDataGenerator)))
 
 (defmulti gen-rands
           "Generates a lazy sequence of random numbers with a distribution defined by the given dispatch.
 
           Examples:
           (gen-rands :uniform) => Lazy seq with mean distribution of 0.5
-          (gen-rands :min 2) => Lazy seq with mean distribution of 0.333
-          (gen-rands :min 3) => Lazy seq with mean distribution of 0.25
-          (gen-rands :min 4) => Lazy seq with mean distribution of 0.20
-          (gen-rands :max 2) => Lazy seq with mean distribution of 0.667
-          (gen-rands :max 3) => Lazy seq with mean distribution of 0.75
-          (gen-rands :max 4) => Lazy seq with mean distribution of 0.80"
+          (gen-rands :low-pass 2) => Lazy seq with mean distribution of 0.333
+          (gen-rands :low-pass 3) => Lazy seq with mean distribution of 0.25
+          (gen-rands :low-pass 4) => Lazy seq with mean distribution of 0.20
+          (gen-rands :high-pass 2) => Lazy seq with mean distribution of 0.667
+          (gen-rands :high-pass 3) => Lazy seq with mean distribution of 0.75
+          (gen-rands :high-pass 4) => Lazy seq with mean distribution of 0.80"
           {:arglists '([dist & args])}
           (fn [dist & args] dist))
 
+(defn- gen-n-rands [count] (take count (repeatedly rand)))
+
 (defmethod gen-rands :uniform [dist] (repeatedly rand))
-(defmethod gen-rands :min [dist count] (repeatedly #(apply min (take count (repeatedly rand)))))
-(defmethod gen-rands :max [dist count] (repeatedly #(apply max (take count (repeatedly rand)))))
+(defmethod gen-rands :low-pass [dist count] (repeatedly #(apply min (gen-n-rands count))))
+(defmethod gen-rands :high-pass [dist count] (repeatedly #(apply max (gen-n-rands count))))
+(defmethod gen-rands :mid-pass [dist] (repeatedly #(/ (reduce + (gen-n-rands 2)) 2)))
+(defmethod gen-rands :exponential [dist lambda] (repeatedly #(/ (- (Math/log (- 1 (rand)))) lambda)))
+
+
+; Test Code
+(def rand-gen (RandomDataGenerator.))
+(.nextExponential rand-gen 1.0)
--- test/berwickheights/cac/probability_test.clj
+++ test/berwickheights/cac/probability_test.clj
@@ -9,13 +9,15 @@
 (deftest prob-test
   (testing "Uniform distributions"
     (is (= 50 (find-mean :uniform))))
-  (testing "Min distributions"
-    (is (= 33 (find-mean :min 2)))
-    (is (= 25 (find-mean :min 3)))
-    (is (= 20 (find-mean :min 4)))
-    (is (= 10 (find-mean :min 9))))
-  (testing "Max distributions"
-    (is (= 67 (find-mean :max 2)))
-    (is (= 75 (find-mean :max 3)))
-    (is (= 80 (find-mean :max 4)))
-    (is (= 90 (find-mean :max 9)))))
+  (testing "Low pass distributions"
+    (is (= 33 (find-mean :low-pass 2)))
+    (is (= 25 (find-mean :low-pass 3)))
+    (is (= 20 (find-mean :low-pass 4)))
+    (is (= 10 (find-mean :low-pass 9))))
+  (testing "High pass distributions"
+    (is (= 67 (find-mean :high-pass 2)))
+    (is (= 75 (find-mean :high-pass 3)))
+    (is (= 80 (find-mean :high-pass 4)))
+    (is (= 90 (find-mean :high-pass 9))))
+  (testing "Mid pass distributions"
+    (is (= 50 (find-mean :mid-pass)))))
