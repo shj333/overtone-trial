fee22b55c2117aa873c303e324b94a01744d1a99
stuart <stuartj@berwickheights.com>
2016-05-11 09:23:14 -0400

    Upgrade to Overtone 0.10.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [overtone "0.10-SNAPSHOT"]
+                 [overtone "0.10.1"]
                  [leipzig "0.9.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/core.logic "0.8.10"]
