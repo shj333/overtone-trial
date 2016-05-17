8ebf51215b811987ccb16027f6de596a437fe380
stuart <stuartj@berwickheights.com>
2016-05-17 08:28:48 -0400

    SC Book Chap 16 Microsounds 1. Use CAC Patterns lib 2. Added more examples from book

--- src/berwickheights/sc_book/chapter16_microsound.clj
+++ src/berwickheights/sc_book/chapter16_microsound.clj
@@ -1,52 +1,24 @@
 (ns berwickheights.sc-book.chapter16-microsound
-  (:use [overtone.core]))
+  (:use [overtone.core])
+  (:require [berwickheights.cac.overtone.patterns :as pat]))
 
+; Example 16.1 Short grain durations
 ; Gabor grain, Gaussian-shaped envelope
 (defsynth gabor [out-bus [0 :ir] freq [440 :ir] sustain [1 :ir] pan [0.0 :ir] amp [0.1 :ir] width [0.25 :ir]]
           (let [env (lf-gauss:ar sustain width :loop 0 :action FREE)
                 half-pi (* 0.5 (. Math PI))
                 son (* env (f-sin-osc:ar freq half-pi))]
             (offset-out:ar out-bus (pan2:ar son pan amp))))
-; (gabor)
+;(gabor)
 
 ; Approximation of above with a sine-shaped envelope
 (defsynth gabor1 [out-bus [0 :ir] freq [440 :ir] sustain [1 :ir] pan [0.0 :ir] amp [0.1 :ir]]
           (let [snd (f-sin-osc:ar freq)
                 env (env-gen:ar (env-sine sustain amp) :action FREE)]
             (offset-out:ar out-bus (pan2:ar (* snd env) amp))))
-; (gabor1)
+;(gabor1)
 
 
-(defn- get-value
-  [f param]
-  (if (seq? param)
-    (f param)
-    param))
-
-(defn this-synth-params
-  [params-map]
-  (flatten (for [[k v] params-map] [k (get-value first v)])))
-
-(defn next-synth-params
-  [params-map]
-  (into {} (for [[k v] params-map] [k (get-value next v)])))
-
-(defn defpbind
-  [beat data]
-  (let [{:keys [synth params dur]} @data
-        this-synth (get-value first synth)
-        this-params (this-synth-params params)
-        next-beat (+ beat dur)]
-    (println (pr-str "beat: " beat ", next-beat: " next-beat))
-    (when (not-any? nil? this-params)
-      (at beat (apply this-synth this-params))
-      (swap! data assoc :synth (get-value next synth) :params (next-synth-params params))
-      (apply-by next-beat #'defpbind [next-beat data]))))
-
-(defn defpat
-  [data & args]
-  (swap! data assoc :params (apply assoc (:params @data) args))
-  key)
 
 (def gabor-data (atom {:synth  gabor
                        :params {:out-bus 0
@@ -55,14 +27,39 @@
                                 :pan     0.0
                                 :amp     (cycle [0.1, 0.1])
                                 :width   0.25
+                                :dur     1000
                                 }
-                       :dur    1000
                        }))
+; Short grain, then 2x and 4x louder
+;(pat/defpbind (now) gabor-data)
+;(pat/defpat gabor-data :sustain (cycle [0.001 0.1]) :amp (cycle [0.2, 0.1]))
+;(pat/defpat gabor-data :sustain (cycle [0.001 0.1]) :amp (cycle [0.4, 0.1]))
+;(pat/defpat gabor-data :amp nil)
 
-(defpbind (now) gabor-data)
-; Short grain 2x louder
-(defpat gabor-data :sustain (cycle [0.001 0.1]) :amp (cycle [0.2, 0.1]))
-; Short grain 4x louder
-(defpat gabor-data :sustain (cycle [0.001 0.1]) :amp (cycle [0.4, 0.1]))
-; Stop it
-(defpat gabor-data :amp nil)
+
+; Pitch to colored click
+(def grain-data (atom {:synth  gabor
+                       :params {:out-bus 0
+                                :freq    1000
+                                :sustain 0.02
+                                :pan     0.0
+                                :amp     0.2
+                                :dur     500
+                                }
+                       }))
+;(pat/defpbind (now) grain-data)
+;(pat/defpat grain-data :sustain (repeatedly #(/ 10 (pat/pat-key grain-data :freq))))
+;(pat/defpat grain-data :sustain (repeatedly #(/ 5 (pat/pat-key grain-data :freq))))
+;(pat/defpat grain-data :sustain (repeatedly #(/ 3 (pat/pat-key grain-data :freq))))
+;(pat/defpat grain-data :sustain (repeatedly #(/ 2 (pat/pat-key grain-data :freq))))
+;(pat/defpat grain-data :sustain (repeatedly #(/ 1 (pat/pat-key grain-data :freq))))
+;(pat/defpat grain-data :amp nil)
+
+
+; Successively shorter, end
+;(pat/defpat grain-data :sustain (map #(/ % (pat/pat-key grain-data :freq)) (range 10 0 -1)))
+
+
+; Random drift of grain duration
+;(pat/defpat grain-data :dur 100 :sustain (repeatedly #(/ (rand-int 10) (pat/pat-key grain-data :freq))))
+;(pat/defpat grain-data :amp nil)
